6caba0ac724517454fd75f79e4bfe4b2e6151682
Coner Murphy <coner@conermurphy.com>
2021-10-09 08:09:04 +0100

    disabled eslint in places to avoid errors

--- src/utils/lookupCountry.js
+++ src/utils/lookupCountry.js
@@ -1,14 +1,16 @@
-async function lookupCountry({ latitude: lat, longitude: long }) {
+async function lookupCountry({ latitude, longitude }) {
   // Prefix the env file with REACT_APP for react to find it
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
 
   const locationData = await fetch(URL).then((res) => res.json());
 
-  const [{ address_components: addressComponents }] = locationData.results.filter(({ types }) => types.includes('country'));
+  /* eslint-disable */
+  const [{ address_components }] = locationData.results.filter(({ types }) => types.includes('country'));
 
-  const [{ short_name: countryCode }] = addressComponents;
-
-  return countryCode;
+  const [{ short_name }] = address_components;
+  
+  return short_name;
+  /* eslint-enable */
 }
 
 export default lookupCountry;
